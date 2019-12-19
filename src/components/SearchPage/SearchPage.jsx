a941166b66af7f215784c0dca5162ee2cd14af14
Dustin Fedie <dlfedie@gmail.com>
2019-12-19 11:27:32 -0600

    adds most of the search page, just need to make the add to checkout button work. and work on the checkout page

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -9,8 +9,10 @@ class SearchPage extends Component {
         searchText: ''
     }
 
-    searchForGame = () => {
-        console.log('clicked on the old button');
+    searchForGame = (event) => {
+        // makes page not reload on submit
+        event.preventDefault();
+        console.log('clicked on the old button. or hit enter, now that we added form..');
         this.props.dispatch({
             type: 'SEARCH_FOR_GAME',
             payload: {searchText: this.state.searchText}
@@ -29,20 +31,51 @@ class SearchPage extends Component {
     }
 
     render() {
+
+        let searchResultsList = this.props.searchResults.map((game, index) => {
+            return (
+                <article className="card">
+                    <img src={game.image.small_url} alt={game.name} />
+                    <div className="card-content">
+                        <h2>{game.name}</h2>
+                        <p>{game.deck}</p>
+                        <h2>Platforms:</h2>
+                        {game.platforms.map((system) => {
+                            return (
+                                <h3>{system.name}</h3>
+                            )
+                        })}
+                        <button>Add to Checkout!</button>
+                    </div>
+                </article>
+            )
+        })
+
         return (
-            <div>
+            <>
+            <div className="top-part-of-search">
                 <h1>Search</h1>
-                <input onChange={this.setSearchText} />
-                <button onClick={this.searchForGame}>Search!</button>
-                {JSON.stringify(this.state.searchText)}
+                <form>
+                    <input onChange={this.setSearchText} className="search-input" />
+                    <button onClick={this.searchForGame} className="search-button">Search!</button>
+                </form>
+                {/* {JSON.stringify(this.state.searchText)} */}
             </div>
+            <div>
+                <section className="cards">
+                        {this.props.searchResults &&
+                            searchResultsList}
+                </section>
+                {JSON.stringify(this.props.searchResults)}
+            </div>
+            </>
         )
     }
 }
 
 const mapStateToProps = (reduxStore) => {
     return {
-        searchResults: reduxStore
+        searchResults: reduxStore.searchResults.searchReducer,
     }
 }
 
--- src/redux/reducers/searchResults.jsx
+++ src/redux/reducers/searchResults.jsx
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 
-const searchReducer = (state = [], action) => {
+const searchReducer = (state = [{
+    name: 'Metroid Prime', deck: 'Take control of Samus Aran in her first 3D adventure as she battles the Space Pirates on Tallon IV while uncovering the mysterious disappearance of its inhabitants.', image: {
+        small_url: 'https://www.giantbomb.com/api/image/scale_small/2550128-primeclean.jpg' }, platforms: [] }], action) => {
     switch (action.type) {
         case 'SET_SEARCH_RESULTS':
             return action.payload;
